7fb5c3118f7c96c683db26973eaafad09c1cb80b
sanskar-23 <skapoor_be20@thapar.edu>
2024-03-05 02:10:50 +0530

    Created Login Route

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,9 @@
 import userModel from "../models/userModel.js";
 import { hashPassword, comparePassword } from "./../helpers/authHelper.js";
+import JWT from "jsonwebtoken";
 
-export const registerController = async () => {
+// REGISTER || POST
+export const registerController = async (req, res) => {
   try {
     const { name, email, password, phone, address } = req.body;
     if (!name) return res.send({ error: "Name is Required" });
@@ -41,6 +43,61 @@ export const registerController = async () => {
     res.status(500).send({
       success: false,
       message: "Error in Registration",
+      error,
+    });
+  }
+};
+
+// LOGIN || POST
+export const loginController = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.status(404).send({
+        success: false,
+        message: "Invalid email or password",
+      });
+    }
+
+    const user = await userModel.findOne({ email });
+
+    if (!user) {
+      return res.status(404).send({
+        success: false,
+        message: "Email is not registered",
+      });
+    }
+
+    const match = await comparePassword(password, user.password);
+    if (!match) {
+      return res.status(200).send({
+        success: false,
+        message: "Incorrect Password",
+      });
+    }
+
+    //token
+
+    const token = await JWT.sign({ _id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+    res.status(200).send({
+      success: true,
+      message: "Login Successfully",
+      user: {
+        name: user.name,
+        email: user.email,
+        phone: user.phone,
+        address: user.address,
+        role: user.role,
+      },
+      token,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "Error in Login",
       error,
     });
   }
