764cd64044acbbfed9b971dbee18f3e5ecbfede5
sanskar-23 <skapoor_be20@thapar.edu>
2024-03-04 14:27:31 +0530

    First Commit

---
+++ server.js
@@ -0,0 +1,26 @@
+import express from "express";
+import colors from "colors";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import connectDB from "./config/db.js";
+
+const app = express();
+dotenv.config();
+
+const PORT = process.env.PORT || 8000;
+
+connectDB();
+
+// Middlewares
+app.use(express.json());
+app.use(morgan("dev"));
+
+app.get("/", (req, res) => {
+  res.send("<h1>Welcome to shopping</h1>");
+});
+
+app.listen(PORT, () => {
+  console.log(
+    `Server is Running on ${process.env.DEV_MODE} mode on ${PORT}`.bgCyan.white
+  );
+});
