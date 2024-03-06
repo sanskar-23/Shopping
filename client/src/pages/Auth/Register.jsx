f8aed929dd3e5b7818a73919218613ab5b925b33
sanskar-23 <skapoor_be20@thapar.edu>
2024-03-06 17:40:27 +0530

    Created Register Page

---
+++ client/src/pages/Auth/Register.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+import Layout from "../../components/Layout/Layout";
+
+const Register = () => {
+  return (
+    <Layout title={"Register Page - Shopping"}>
+      <div className="register">
+        <h1>Register Page</h1>
+        <form>
+          <div className="mb-3">
+            <input
+              type="text"
+              className="form-control"
+              id="exampleInputEmail1"
+              placeholder="Enter Your Name"
+              required
+              autoFocus
+            />
+          </div>
+          <div className="mb-3">
+            <input
+              type="email"
+              className="form-control"
+              id="exampleInputEmail1"
+              placeholder="Enter Your Email"
+              required
+            />
+          </div>
+          <div className="mb-3">
+            <input
+              type="password"
+              className="form-control"
+              id="exampleInputPassword1"
+              placeholder="Enter Your Password"
+              required
+            />
+          </div>
+          <div className="mb-3">
+            <input
+              type="text"
+              className="form-control"
+              id="exampleInputPassword1"
+              placeholder="Enter your Phone"
+              required
+            />
+          </div>
+          <div className="mb-3">
+            <input
+              type="text"
+              className="form-control"
+              id="exampleInputPassword1"
+              placeholder="Enter Your Address"
+              required
+            />
+          </div>
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
+        </form>
+      </div>
+    </Layout>
+  );
+};
+
+export default Register;
