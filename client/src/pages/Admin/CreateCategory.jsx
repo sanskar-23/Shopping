fac7b0d46f7abd1df63a7a917b5c989edc8c137c
sanskar-23 <skapoor_be20@thapar.edu>
2024-03-10 04:12:15 +0530

    Started Creating Admin Dashboard

---
+++ client/src/pages/Admin/CreateCategory.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Layout from "../../components/Layout/Layout";
+
+const CreateCategory = () => {
+  return <Layout>CreateCategory</Layout>;
+};
+
+export default CreateCategory;
---
+++ client/src/pages/Admin/CreateProduct.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Layout from "../../components/Layout/Layout";
+
+const CreateProduct = () => {
+  return <Layout>CreateProduct</Layout>;
+};
+
+export default CreateProduct;
