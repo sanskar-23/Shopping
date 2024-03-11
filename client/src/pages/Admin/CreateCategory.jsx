4a9337616994e7874599ea61fcecd4140ff7441a
sanskar-23 <skapoor_be20@thapar.edu>
2024-03-12 02:44:59 +0530

    Created Products Page CRUD Operation

--- client/src/pages/Admin/CreateCategory.jsx
+++ client/src/pages/Admin/CreateCategory.jsx
@@ -4,10 +4,14 @@ import AdminMenu from "../../components/Layout/AdminMenu";
 import toast from "react-hot-toast";
 import axios from "axios";
 import CategoryForm from "../../components/Form/CategoryForm";
+import { Modal } from "antd";
 
 const CreateCategory = () => {
   const [categories, setCategories] = useState([]);
   const [name, setName] = useState("");
+  const [visible, setVisible] = useState(false);
+  const [selected, setSelected] = useState(null);
+  const [updatedName, setUpdatedName] = useState("");
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -44,6 +48,46 @@ const CreateCategory = () => {
     getAllCategory();
   }, []);
 
+  // handle update
+  const handleUpdate = async (e) => {
+    e.preventDefault();
+    try {
+      const { data } = await axios.put(
+        `/api/v1/category/update-category/${selected._id}`,
+        { name: updatedName }
+      );
+      if (data?.success) {
+        toast.success(`${updatedName} is updated`);
+        setSelected(null);
+        setUpdatedName("");
+        setVisible(false);
+        getAllCategory();
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // handle update
+  const handleDelete = async (pId) => {
+    try {
+      const { data } = await axios.delete(
+        `/api/v1/category/delete-category/${pId}`,
+        { name: updatedName }
+      );
+      if (data?.success) {
+        toast.success(`Category Deleted`);
+        getAllCategory();
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <Layout title={"Dashboard - Create Category"}>
       <div className="container-fluid m-3 p-3">
@@ -69,22 +113,47 @@ const CreateCategory = () => {
                   </tr>
                 </thead>
                 <tbody>
-                  {categories?.map((c) => (
-                    <>
-                      <tr>
-                        <td key={c?._id}>{c?.name}</td>
-                        <td>
-                          <button className="btn btn-primary ms-2">Edit</button>
-                          <button className="btn btn-danger ms-2">
-                            Delete
-                          </button>
-                        </td>
-                      </tr>
-                    </>
-                  ))}
+                  {categories &&
+                    categories.length > 0 &&
+                    categories?.map((c, index) => (
+                      <>
+                        <tr key={c?._id || index}>
+                          <td>{c?.name}</td>
+                          <td>
+                            <button
+                              className="btn btn-primary ms-2"
+                              onClick={() => {
+                                setVisible(true);
+                                setUpdatedName(c.name);
+                                setSelected(c);
+                              }}
+                            >
+                              Edit
+                            </button>
+                            <button
+                              className="btn btn-danger ms-2"
+                              onClick={() => handleDelete(c._id)}
+                            >
+                              Delete
+                            </button>
+                          </td>
+                        </tr>
+                      </>
+                    ))}
                 </tbody>
               </table>
             </div>
+            <Modal
+              onCancel={() => setVisible(false)}
+              footer={null}
+              open={visible}
+            >
+              <CategoryForm
+                value={updatedName}
+                setValue={setUpdatedName}
+                handleSubmit={handleUpdate}
+              />
+            </Modal>
           </div>
         </div>
       </div>
