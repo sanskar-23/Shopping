b76123b02ea0e54e98372955b76cb31ff88fd482
sanskar-23 <skapoor_be20@thapar.edu>
2024-03-11 17:24:22 +0530

    Created API's for all product

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ import dotenv from "dotenv";
 import morgan from "morgan";
 import authRoutes from "./routes/authRoute.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
+import productRoutes from "./routes/productRoutes.js";
 import connectDB from "./config/db.js";
+
 import cors from "cors";
 
 const app = express();
@@ -22,6 +24,7 @@ app.use(cors());
 //routes
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/category", categoryRoutes);
+app.use("/api/v1/product", productRoutes);
 
 app.get("/", (req, res) => {
   res.send("<h1>Welcome to shopping</h1>");
