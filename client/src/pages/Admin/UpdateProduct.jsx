22c74f49a547aafbd0a4c14f3227f4e8f019be0e
sanskar-23 <skapoor_be20@thapar.edu>
2024-03-13 18:42:54 +0530

    Created UI of Home Page

--- client/src/pages/Admin/UpdateProduct.jsx
+++ client/src/pages/Admin/UpdateProduct.jsx
@@ -40,6 +40,13 @@ const UpdateProduct = () => {
     }
   };
 
+  const truncateDescription = (description, maxLength) => {
+    if (description.length > maxLength) {
+      return description.substring(0, maxLength) + "...";
+    }
+    return description;
+  };
+
   useEffect(() => {
     getSingleProduct();
     // eslint-disable-next-line
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,13 +1,130 @@
 import React from "react";
 import Layout from "../components/Layout/Layout";
-import { useAuth } from "../context/auth";
+import { useState } from "react";
+import toast from "react-hot-toast";
+import axios from "axios";
+import { useEffect } from "react";
+import { Link } from "react-router-dom";
+import { Checkbox, Radio } from "antd";
+import { Prices } from "../components/Prices";
 
 const HomePage = () => {
-  const [auth, setAuth] = useAuth();
+  const [products, setProducts] = useState([]);
+  const [categories, setCategories] = useState([]);
+  const [checked, setChecked] = useState([]);
+  const [radio, setRadio] = useState([]);
+
+  // get all categories
+  const getAllCategory = async () => {
+    try {
+      const { data } = await axios.get("/api/v1/category/get-category");
+      if (data?.success) {
+        setCategories(data?.category);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getAllCategory();
+  }, []);
+
+  // get All products
+  const getAllproducts = async () => {
+    try {
+      const { data } = await axios.get("/api/v1/product/get-product");
+      setProducts(data?.products);
+    } catch (error) {
+      console.log(error);
+      toast.error("Success Went Wrong");
+    }
+  };
+
+  const truncateDescription = (description, maxLength) => {
+    if (description.length > maxLength) {
+      return description.substring(0, maxLength) + "...";
+    }
+    return description;
+  };
+
+  // filter by category
+  const handleFilter = (value, id) => {
+    let all = [...checked];
+    if (value) all.push(id);
+    else all = all.filter((c) => c !== id);
+    setChecked(all);
+  };
+
+  useEffect(() => {
+    getAllproducts();
+  }, []);
+
   return (
-    <Layout title={"Best Offers - Shopping"}>
-      <h1>HomePage</h1>
-      <pre>{JSON.stringify(auth, null, 4)}</pre>
+    <Layout>
+      <img
+        src="/images/background.jpg"
+        className="banner-img"
+        alt="bannerimage"
+        width={"100%"}
+      />
+      <div className="container-fluid mt-3">
+        <div className="row">
+          <div className="col-md-3">
+            <h4 className="text-center">Filter By Category</h4>
+            <div className="d-flex flex-column">
+              {categories?.map((c) => (
+                <div key={c._id} className="card mb-2 category-card">
+                  <div className="card-body">
+                    <Checkbox
+                      onChange={(e) => handleFilter(e.target.checked, c._id)}
+                    >
+                      {c.name}
+                    </Checkbox>
+                  </div>
+                </div>
+              ))}
+            </div>
+            <h4 className="text-center mt-4">Filter By Price</h4>
+            <div className="d-flex flex-column">
+              <Radio.Group
+                onChange={(e) => setRadio(e.target.value)}
+                className="price-filter"
+              >
+                {Prices?.map((p) => (
+                  <div key={p._id} className="price-option">
+                    <Radio value={p.array}>{p.name}</Radio>
+                  </div>
+                ))}
+              </Radio.Group>
+            </div>
+          </div>
+          <div className="col-md-9">
+            <h1 className="text-center"> All Products List</h1>
+            <div className="grid-container">
+              {products?.map((p) => (
+                <Link key={p._id} className="product-link">
+                  <div className="card">
+                    <img
+                      src={`/api/v1/product/product-photo/${p?._id}`}
+                      alt={p?.name}
+                      className="card-img-top"
+                    />
+                    <div className="card-body">
+                      <h5 className="card-title">{p?.name}</h5>
+                      <p className="card-text">
+                        {truncateDescription(p?.description, 100)}
+                      </p>
+                      <button className="btn btn-info m-1">More Details</button>
+                      <button className="btn btn-dark ms-2">ADD TO CART</button>
+                    </div>
+                  </div>
+                </Link>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
     </Layout>
   );
 };
