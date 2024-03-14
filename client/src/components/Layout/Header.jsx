a3c61a740996b2c8f722c73d6f0d4ce514d36164
sanskar-23 <skapoor_be20@thapar.edu>
2024-03-15 03:59:17 +0530

    Changed the UI of NavBar

--- client/src/components/Layout/Header.jsx
+++ client/src/components/Layout/Header.jsx
@@ -21,96 +21,104 @@ const Header = () => {
   };
 
   return (
-    <>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light ">
-        <div className="container-fluid">
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarTogglerDemo01"
-            aria-controls="navbarTogglerDemo01"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
-            <Link to={"/"} className="navbar-brand">
-              <GiShoppingBag className="logo" />
-              Shopping
-            </Link>
-            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-              <SearchInput />
-              <li className="nav-item">
-                <NavLink to={"/"} className="nav-link">
-                  Home
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container">
+        <Link to={"/"} className="navbar-brand">
+          <GiShoppingBag className="logo" />
+          Shopping
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <NavLink to={"/"} className="nav-link" activeClassName="active">
+                Home
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                to={"/category"}
+                className="nav-link"
+                activeClassName="active"
+              >
+                Category
+              </NavLink>
+            </li>
+          </ul>
+          <SearchInput />
+          <ul className="navbar-nav">
+            {auth.user ? (
+              <li className="nav-item dropdown">
+                <NavLink
+                  className="nav-link dropdown-toggle"
+                  to="/"
+                  id="navbarDropdown"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  {auth.user.name}
                 </NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink to={"/category"} className="nav-link ">
-                  Category
-                </NavLink>
-              </li>
-              {!auth.user ? (
-                <>
-                  <li className="nav-item">
-                    <NavLink className="nav-link" to={"/register"}>
-                      Register
-                    </NavLink>
-                  </li>
-                  <li className="nav-item">
-                    <NavLink className="nav-link" to={"/login"}>
-                      Login
-                    </NavLink>
-                  </li>
-                </>
-              ) : (
-                <>
-                  <li className="nav-item dropdown">
+                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <li>
                     <NavLink
-                      className="nav-link dropdown-toggle"
-                      href="#"
-                      role="button"
-                      data-bs-toggle="dropdown"
-                      style={{ border: "none" }}
+                      to={`/dashboard/${
+                        auth.user.role === 1 ? "admin" : "user"
+                      }`}
+                      className="dropdown-item"
                     >
-                      {auth?.user?.name}
+                      Dashboard
                     </NavLink>
-                    <ul className="dropdown-menu">
-                      <li>
-                        <NavLink
-                          to={`/dashboard/${
-                            auth?.user?.role === 1 ? "admin" : "user"
-                          }`}
-                          className="dropdown-item"
-                        >
-                          Dashboard
-                        </NavLink>
-                      </li>
-                      <li>
-                        <NavLink
-                          onClick={handleLogOut}
-                          to="/login"
-                          className="dropdown-item"
-                        >
-                          Logout
-                        </NavLink>
-                      </li>
-                    </ul>
                   </li>
-                </>
-              )}
-              <li className="nav-item">
-                <NavLink className="nav-link" to={"/cart"}>
-                  Cart (0)
-                </NavLink>
+                  <li>
+                    <NavLink
+                      onClick={handleLogOut}
+                      to="/login"
+                      className="dropdown-item"
+                    >
+                      Logout
+                    </NavLink>
+                  </li>
+                </ul>
               </li>
-            </ul>
-          </div>
+            ) : (
+              <>
+                <li className="nav-item">
+                  <NavLink to={"/register"} className="nav-link">
+                    Register
+                  </NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink to={"/login"} className="nav-link">
+                    Login
+                  </NavLink>
+                </li>
+              </>
+            )}
+            <li className="nav-item">
+              <NavLink
+                to={"/cart"}
+                className="nav-link"
+                activeClassName="active"
+              >
+                <span className="me-1">Cart</span>
+                <span className="badge bg-secondary">0</span>
+              </NavLink>
+            </li>
+          </ul>
         </div>
-      </nav>
-    </>
+      </div>
+    </nav>
   );
 };
 
